98836b27918201f33815ab80602de5118b8f2810
Pulkit19-amga <pulkit.a@gbhin.com>
2025-05-20 18:48:54 +0530

    register commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'user_type',
+         'user_type',
+        'dob',
+        'anniversary_date'
     ];
 
     /**
